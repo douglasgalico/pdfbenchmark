6dd7f966da78173072a6faaeaf96aacc0a61b234
Douglas Galico <douglas.galico@gmail.com>
2015-07-01 17:34:33 -0300

    Ajuste de Dependencias

--- pdfbench/setup1/test.php
+++ pdfbench/setup1/test.php
@@ -2,53 +2,30 @@
 
 require_once('tcpdf_include.php');
 require_once('ChartGen.php');
-
+require_once('functions.php');
 
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// set document information
+define('BENCHMARK_PAGES', 50);
+
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 053');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-
-// set default header data
-
-
-// set header and footer fonts
+$pdf->SetAuthor('PDBenchmark');
+$pdf->SetTitle('PDFBenchmark Setup01-1');
+$pdf->SetSubject('TCPDF Com PHP');
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-// set default monospaced font
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-// set margins
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-// set auto page breaks
-//$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-// set image scale factor
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-// set some language-dependent strings (optional)
-if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-    require_once(dirname(__FILE__).'/lang/eng.php');
-    $pdf->setLanguageArray($l);
-}
-
-// ---------------------------------------------------------
-
-// set font
 $pdf->SetFont('times', '', 14);
-$a = 50;
-for($i=0;$i < $a;$i++){
-// add a page
-$pdf->AddPage();
 
+
+for($i=0;$i < BENCHMARK_PAGES;$i++){
+
+$pdf->AddPage();
 $text = loadTemplate('teste.php',['ta' => '438gdf43']);
 $pdf->writeHTML($text, true, 0, true, 0);
 }
@@ -56,14 +33,3 @@ $pdf->writeHTML($text, true, 0, true, 0);
 $pdf->Output(dirname(__FILE__) .DIRECTORY_SEPARATOR.'output'.DIRECTORY_SEPARATOR.'Outputfile.pdf', 'F');
 
 
-
-
-
-
-function loadTemplate($page, $data = array()) {
-    extract($data);
-    $path = 'templates'. DIRECTORY_SEPARATOR . $page;
-    ob_start();
-    include $path;
-    return ob_get_clean();
-}
